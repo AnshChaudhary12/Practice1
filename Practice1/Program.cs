8cbf440293856e5786fba82e6dc7b1aa08ecb1ba
HP <HP@LAPTOP-05I8OOOO>
2023-08-13 11:26:41 +0530

    Add project files.

---
+++ Practice1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any integer : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+
+
+        }
+    }
+}
